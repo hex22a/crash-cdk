7bc9ce9290eb322658d516ea9145834e0d4867f3
Crash <hex22a@gmail.com>
2019-07-28 19:17:28 +0300

    almost

--- lib/crash-cdk-stack.ts
+++ lib/crash-cdk-stack.ts
@@ -1,9 +1,66 @@
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2');
 
 export class CrashCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const Ubuntu18 = 'ami-07d0cf3af28718ef8';
+    const everyone = "0.0.0.0/0";
+    const everyoneV6 = "::/0";
+
+    const httpsTraffic = {
+      cidrIp: everyone,
+      fromPort: 443,
+      ipProtocol: "TCP",
+      toPort: 443
+    };
+
+    const customHttpsTraffic = {
+      cidrIp: everyone,
+      fromPort: 943,
+      ipProtocol: "TCP",
+      toPort: 943
+    };
+
+    const sshTraffic = {
+      cidrIp: everyone,
+      fromPort: 22,
+      ipProtocol: "TCP",
+      toPort: 22
+    };
+
+    const vpnTcpTraffic = {
+      cidrIp: everyone,
+      fromPort: 1199,
+      ipProtocol: "TCP",
+      toPort: 1199
+    };
+
+    const vpnUdpTraffic = {
+      cidrIp: everyone,
+      fromPort: 1194,
+      ipProtocol: "udp",
+      toPort: 1194
+    };
+
+    const securityGroup = new ec2.CfnSecurityGroup(this, 'vpn_sg', {
+      groupName: 'vpn_sg',
+      groupDescription: 'Allows TCP traffic for VPN connection',
+      securityGroupIngress: [
+        httpsTraffic,
+        customHttpsTraffic,
+        sshTraffic,
+        vpnTcpTraffic,
+        vpnUdpTraffic
+      ]
+    });
+
+    new ec2.CfnInstance(this, 'UbuntuVPN', {
+      imageId: Ubuntu18,
+      instanceType: 't2.micro',
+      monitoring: false,
+      securityGroupIds: [securityGroup.attrGroupId]
+    });
   }
 }
