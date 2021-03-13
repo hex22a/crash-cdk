79579ff845c75ca6e2ffffefa9427f9b15091ff1
Crash <hex22a@gmail.com>
2021-03-13 22:10:23 +0300

    add bitwarden setup

--- bin/crash-cdk.ts
+++ bin/crash-cdk.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { CrashCdkStack } from '../lib/crash-cdk-stack';
+import { PasswordManagetStack } from '../lib/password-manager-stack';
 
 const app = new cdk.App();
 new CrashCdkStack(app, 'CrashCdkStack');
+new PasswordManagetStack(app, 'PasswordManagetStack');
