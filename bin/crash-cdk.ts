d2d355021c169228c1e1ea640e8689b1ff8b7720
Crash <1903291+hex22a@users.noreply.github.com>
2024-09-03 20:26:24 +0400

    Project EoL

--- bin/crash-cdk.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CrashCdkStack } from '../lib/crash-cdk-stack';
-// import { PasswordManagerStack } from '../lib/password-manager-stack';
-
-const app = new cdk.App();
-new CrashCdkStack(app, 'CrashCdkStack');
-// new PasswordManagerStack(app, 'PasswordManagerStack');
